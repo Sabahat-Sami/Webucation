1f8d5eb242880aacc6709d56267a839839ba4b8f
Tanvi <tanvi.rahman@nyu.edu>
2023-10-16 10:21:31 -0400

    started frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Register from './pages/Register.js'
+import Login from './pages/Login.js'
+import Home from './pages/Home.js'
+import Admin from './pages/Admin.js'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar.js'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App bg-gradient-to-b from-[#BBC2FA] to-white'>
+      <Navbar />
+      <Router>
+        <Routes>
+          <Route exact path='/' element={<Home />}></Route>
+          <Route exact path='/register' element={<Register />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/admin' element={<Admin />} />
+          <Route exact path='*' element={<Home />}></Route>
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App

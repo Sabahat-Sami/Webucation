af01868ae463a514afb04ac595ab31e2ba6072aa
Tanvi <tanvi.rahman@nyu.edu>
2023-12-04 19:56:20 -0500

    edit profile frontend setup

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useState, useRef } from 'react'
 
 export default function EditProfile() {
-    const user = {
+      // @ Ivan : the user below should be set to whatever user is logged in at the moment
+    let user = {
         email: "sabahatsami@nyu.edu",
         password: "password",
         fname: "Sabahat",
@@ -9,26 +11,46 @@ export default function EditProfile() {
         phone_num: "123-456-7890",
         grad: "Spring 2024",
         numFriends: 14,
-        about: "I am a computer science major interested in Psychology stuff. I'm from Queens, New York but my family felt like leaving \
-                to Ohio for some reason. I made this stupid web application for a design project course at NYU, but it's turning out to \
-                be a pretty awful experience. I can't wait for this semester to be over."
+        about: "I am a computer science major interested in Psychology stuff. I'm from Queens, New York but my family felt like leaving to Ohio for some reason. I made this stupid web application for a design project course at NYU, but it's turning out to be a pretty awful experience. I can't wait for this semester to be over.",
+        pfp: "https://t4.ftcdn.net/jpg/03/32/59/65/360_F_332596535_lAdLhf6KzbW6PWXBWeIFTovTii1drkbT.jpg"
+    }
+    
+    const inputRef = useRef(null);
+    const [image, setImage] = useState(user.pfp);
 
+    const handleImageClick = () => {
+      inputRef.current.click();
+    }
+    
+    function handleImageChange(e) {
+      const img = URL.createObjectURL(e.target.files[0])// @ Ivan : modify the user's pfp to this img variable
+      setImage(img);
     }
 
   return (
     <div>
         <br/>
-            <img src="https://media.licdn.com/dms/image/C4D03AQFwzb4_V7DTPw/profile-displayphoto-shrink_800_800/0/1623803824719?e=1706140800&v=beta&t=JI_0IXhNQfXo8NzcOMGl23kEpeHrKiyRHytXWE9L_HQ"
-               className='float-right mt-[8%] rounded-full w-[25%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400'/>
+            <div onClick={handleImageClick} className=''>
+                <img  src={image}
+                      className='float-right mt-[8%] rounded-full w-[25%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400 hover:cursor-pointer hover:opacity-75 hover:scale-110'/>
+                <input
+                  className='float-right mt-8 px-3 py-3 rounded-full w-[25%] ml-[40%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400'
+                  type='file'
+                  ref={inputRef}
+                  style={{display: "none"}}
+                  onChange={handleImageChange}>
+                </input>
+            </div>
+
             <p className='ml-[15%] mt-[8%] underline text-5xl'>{user.fname + " " + user.lname}</p>
-            <p className='ml-[15%] mt-8 text-2xl'>Expected Graduation: {user.grad}</p>
-            <a href='/network'><p className='ml-[15%] mt-4 text-2xl hover:underline hover:text-blue-500'>{user.numFriends} connections</p></a>
-            <p className='ml-[15%] mt-8 text-3xl underline'>Contact Info</p>
-            <p className='ml-[15%] mt-4 text-2xl '>Email: {user.email}</p>
-            <p className='ml-[15%] mt-2 text-2xl '>Phone: {user.phone_num}</p>
+            <p className='ml-[15%] mt-2 text-2xl '>Expected Graduation: <textarea className='ml-6 mt-2 text-2xl h-11 rounded-xl resize-x px-2 py-2'>{user.grad}</textarea></p> 
+            <p className='ml-[15%] mt-4 text-2xl'>{user.numFriends} connections</p>
+            <p className='ml-[15%] mt-3 text-3xl underline'>Contact Info</p>
+            <p className='ml-[15%] mt-2 text-2xl '>Email: <textarea className='ml-6 mt-2 text-2xl h-11 rounded-xl resize-x w-[30%] px-2 py-2'>{user.email}</textarea></p> 
+            <p className='ml-[15%] mt-2 text-2xl '>Phone: <textarea className='ml-4 mt-2 text-2xl h-11 rounded-xl resize-x w-[30%] px-2 py-2'>{user.phone_num}</textarea></p> 
             <p className='ml-[15%] mt-8 text-3xl underline'>About Me</p>
-            <p className='ml-[15%] mr-[15%] mt-4 text-2xl'>{user.about}</p>
-            <a href='/register'>
+            <textarea className='ml-[15%] mr-[15%] mt-4 text-2xl w-[65%] h-[20%] px-4 py-4 rounded-2xl resize-y'>{user.about}</textarea>
+            <a href='/profile'>
                 <button className='ml-[45%] px-8 ml-12 mt-4 py-3 bg-[#a6aff8] text-xl text-white rounded-full hover:bg-blue-800 hover:py-4 hover:px-10 hover:ml-[44.5%]'>
                     Save Changes
                 </button>
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,28 +1,26 @@
 import React from 'react'
 
 export default function Profile() {
-    const user = {
-        email: "sabahatsami@nyu.edu",
-        password: "password",
-        fname: "Sabahat",
-        lname: "Sami",
-        phone_num: "123-456-7890",
-        grad: "Spring 2024",
-        numFriends: 14,
-        about: "I am a computer science major interested in Psychology stuff. I'm from Queens, New York but my family felt like leaving \
-                to Ohio for some reason. I made this stupid web application for a design project course at NYU, but it's turning out to \
-                be a pretty awful experience. I can't wait for this semester to be over."
-
-    }
+  let user = {
+    email: "sabahatsami@nyu.edu",
+    password: "password",
+    fname: "Sabahat",
+    lname: "Sami",
+    phone_num: "123-456-7890",
+    grad: "Spring 2024",
+    numFriends: 14,
+    about: "I am a computer science major interested in Psychology stuff. I'm from Queens, New York but my family felt like leaving to Ohio for some reason. I made this stupid web application for a design project course at NYU, but it's turning out to be a pretty awful experience. I can't wait for this semester to be over.",
+    pfp: "https://t4.ftcdn.net/jpg/03/32/59/65/360_F_332596535_lAdLhf6KzbW6PWXBWeIFTovTii1drkbT.jpg"
+  }
 
   return (
     <div>
         <br/>
-            <img src="https://media.licdn.com/dms/image/C4D03AQFwzb4_V7DTPw/profile-displayphoto-shrink_800_800/0/1623803824719?e=1706140800&v=beta&t=JI_0IXhNQfXo8NzcOMGl23kEpeHrKiyRHytXWE9L_HQ"
+            <img src={user.pfp}
                className='float-right mt-[8%] rounded-full w-[25%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400'/>
             <p className='ml-[15%] mt-[8%] underline text-5xl'>{user.fname + " " + user.lname}</p>
             <p className='ml-[15%] mt-8 text-2xl'>Expected Graduation: {user.grad}</p>
-            <a href='/network'><p className='ml-[15%] mt-4 text-2xl hover:underline hover:text-blue-500'>{user.numFriends} connections</p></a>
+            <p className='ml-[15%] mt-4'><a href='/network' className='text-2xl hover:underline hover:text-blue-500'>{user.numFriends} connections</a></p>
             <p className='ml-[15%] mt-8 text-3xl underline'>Contact Info</p>
             <p className='ml-[15%] mt-4 text-2xl '>Email: {user.email}</p>
             <p className='ml-[15%] mt-2 text-2xl '>Phone: {user.phone_num}</p>

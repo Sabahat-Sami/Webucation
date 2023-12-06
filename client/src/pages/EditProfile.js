e543e40f326ef70fd33fe067a02e0e74d34ed26b
Tanvi <tanvi.rahman@nyu.edu>
2023-12-06 09:04:54 -0500

    Update remove avatar button

--- client/src/pages/EditProfile.js
+++ client/src/pages/EditProfile.js
@@ -10,7 +10,7 @@ export default function EditProfile() {
 
     const inputRef = useRef(null);
     const navigate = useNavigate()
-    const [pfp, setPfp] = useState("");
+    const [pfp, setPfp] = useState("")
     const [newPfp, setNewPfp] = useState(null)
     const [email, setEmail] = useState("");
     const [name, setName] = useState("");
@@ -120,13 +120,20 @@ export default function EditProfile() {
 
 
     
-
   return (
     <div>
         <br/>
             <div onClick={handleImageClick} className=''>
+                <button className='float-right px-4 mt-[8%] ml-[50%] mr-[20%] py-3 bg-red-300 text-sm text-white rounded-full hover:bg-red-800 hover:py-4'
+                onClick={delete_pfp} 
+                >
+                  Remove avatar
+                </button>
+                <p className='absolute ml-[55%] mr-[25%] mt-[20%] w-[25%] h-[15%] text-xl opacity-0 hover:cursor-pointer hover:opacity-100 duration-300 inset-0 z-10 flex justify-center items-center font-semibold'>
+                  Change photo
+                </p>
                 <img  src={pfp}
-                      className='float-right mt-[8%] rounded-full w-[25%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400 hover:cursor-pointer hover:opacity-75 hover:scale-110'/>
+                      className='float-right mb-8 z-0 rounded-full w-[25%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400 hover:cursor-pointer hover:opacity-75 hover:scale-110 '/>
                 <input
                   className='float-right mt-8 px-3 py-3 rounded-full w-[25%] ml-[40%] mr-[20%] outline outline-offset-2 outline-4 outline-gray-400'
                   type='file'
@@ -142,13 +149,14 @@ export default function EditProfile() {
             <p className='ml-[15%] mt-3 text-3xl underline'>Contact Info</p>
             <p className='ml-[15%] mt-2 text-2xl '>Email: <textarea className='ml-6 mt-2 text-2xl h-11 rounded-xl resize-x w-[30%] px-2 py-2' value = {email} onChange= {e => {setEmail(e.target.value)}}></textarea></p> 
             <p className='ml-[15%] mt-2 text-2xl '>Phone: <textarea className='ml-4 mt-2 text-2xl h-11 rounded-xl resize-x w-[30%] px-2 py-2' value = {phone_num} onChange= {e => {setNum(e.target.value)}}></textarea></p> 
-            <button className='ml-[60.6%] px-8 ml-12 mt-4 py-3 bg-[#a6aff8] text-xl text-white rounded-full hover:bg-blue-800 hover:py-4 hover:px-10' onClick={delete_pfp} >
-                Delete profile picture
-            </button>
             <p className='ml-[15%] mt-8 text-3xl underline'>About Me</p>
-            <textarea className='ml-[15%] mr-[15%] mt-4 text-2xl w-[65%] h-[20%] px-4 py-4 rounded-2xl resize-y' value = {about} onChange= {e => {setAbout(e.target.value)}}></textarea>
+            <textarea className='ml-[15%] mr-[15%] mt-4 text-2xl w-[65%] h-[20%] px-4 py-4 rounded-2xl resize-y' value = {about} 
+            onChange= {e => {setAbout(e.target.value)}}
+            ></textarea>
             <a href='/profile'>
-                <button className='ml-[44.5%] px-8 ml-12 mt-4 py-3 bg-[#a6aff8] text-xl text-white rounded-full hover:bg-blue-800 hover:py-4 hover:px-10' onClick={saveProfile} >
+                <button className='ml-[44.5%] px-8 ml-12 mt-4 py-3 bg-[#a6aff8] text-xl text-white rounded-full hover:bg-blue-800 hover:py-4 hover:px-10' 
+                onClick={saveProfile} 
+                >
                     Save Changes
                 </button>
             </a> 
